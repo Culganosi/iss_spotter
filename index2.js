3ca86f8cf460e2c772d65afe25ba7ed9ea444ced
Culganosi <culgan_@hotmail.com>
2022-03-18 20:51:40 -0400

    iss_promised.js and index2.js pushed

---
+++ index2.js
@@ -0,0 +1,12 @@
+// index2.js
+const {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation} = require('./iss_promised');
+const { printPassTimes } = require('./index');
+
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
